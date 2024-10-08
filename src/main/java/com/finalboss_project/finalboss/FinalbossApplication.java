eaca28d8d8d8be31e601d5cb572d55d08145b36a
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-08 11:24:23 -0500

    :tada: Inicio de proyecto :tada:

---
+++ src/main/java/com/finalboss_project/finalboss/FinalbossApplication.java
@@ -0,0 +1,13 @@
+package com.finalboss_project.finalboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalbossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalboss_project/finalboss/FinalbossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalboss_project.finalboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalbossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
