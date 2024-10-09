3beae791d28cfc5945b4471ca3b018dd7fac5cd0
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-09 06:51:51 -0500

    :sparkles: Archivos de configuración spring security comentados :sparkles:

---
+++ src/main/java/com/finalboss_project/finalboss/empresa/domain/entity/Empresa.java
@@ -0,0 +1,32 @@
+package com.finalboss_project.finalboss.empresa.domain.entity;
+
+import com.finalboss_project.finalboss.tipoEmpresa.domain.entity.TipoEmpresa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+    @Id
+    @Column(name = "empresaId")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "tipoEmpresaId")
+    private TipoEmpresa tipoEmpresa;
+}
