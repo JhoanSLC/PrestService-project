1b9e953ceaaa6ec7237bdbf97eeca6ca81704400
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-09 13:54:14 -0500

    :construction: Ciudad entity :construction:

---
+++ src/main/java/com/finalboss_project/finalboss/ciudad/domain/entity/Ciudad.java
@@ -0,0 +1,36 @@
+package com.finalboss_project.finalboss.ciudad.domain.entity;
+
+import com.finalboss_project.finalboss.region.domain.entity.Region;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ciudad",uniqueConstraints = @UniqueConstraint(columnNames = {"nombre","regionId"}))
+public class Ciudad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "regionId")
+    private Region region;
+
+}
