2e3c50c92fc1ebd49663e9721ee5a82ce6939ac0
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-10 07:30:54 -0500

    :construction: Inicio de entidad tipoTelefono :construction:

--- src/main/java/com/finalboss_project/finalboss/tipoTelefono/tipoTelefono.java
+++ src/main/java/com/finalboss_project/finalboss/tipoTelefono/tipoTelefono.java
@@ -1,5 +1,12 @@
 package com.finalboss_project.finalboss.tipoTelefono;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class tipoTelefono {
 
 }
