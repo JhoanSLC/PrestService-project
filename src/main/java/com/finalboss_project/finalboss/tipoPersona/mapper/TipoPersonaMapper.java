f1edb65b5e9855392da7b2ceab1fa73d065e76d7
JuanFParedes <juanfelipeparedes24@gmail.com>
2024-10-16 12:50:44 -0500

    :construction: Crud TipoTelefono :construction:

---
+++ src/main/java/com/finalboss_project/finalboss/tipoPersona/mapper/TipoPersonaMapper.java
@@ -0,0 +1,24 @@
+package com.finalboss_project.finalboss.tipoPersona.mapper;
+
+import com.finalboss_project.finalboss.tipoPersona.domain.entity.TipoPersona;
+import com.finalboss_project.finalboss.tipoPersona.dto.TipoPersonaDto;
+
+public class TipoPersonaMapper {
+    
+    public static TipoPersonaDto toDto(TipoPersona tipoPersona) {
+        return new TipoPersonaDto(
+            tipoPersona.getId(),
+            tipoPersona.getNombre()
+        );
+    }
+
+    public static TipoPersona toEntity(TipoPersonaDto dto) {
+        return new TipoPersona(
+            dto.getId(),
+            dto.getNombre()
+        );
+    }
+    
+}
+
+
