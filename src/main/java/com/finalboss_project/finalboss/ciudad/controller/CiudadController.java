9d1e523dbb8cf7ec374b4eb1d440acb4067cf8ff
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-17 10:44:25 -0500

    :sparkles: Safe merge from juan :sparkles:

--- src/main/java/com/finalboss_project/finalboss/ciudad/controller/CiudadController.java
+++ src/main/java/com/finalboss_project/finalboss/ciudad/controller/CiudadController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalboss_project.finalboss.ciudad.domain.service.ICiudadService;
-import com.finalboss_project.finalboss.ciudad.dto.EstadoAprobacionDto;
+import com.finalboss_project.finalboss.ciudad.dto.CiudadDto;
 
 import jakarta.validation.Valid;
 
@@ -29,27 +29,27 @@ public class CiudadController {
     private ICiudadService service;
 
     @PostMapping 
-    public ResponseEntity<EstadoAprobacionDto> create(@Valid @RequestBody EstadoAprobacionDto dto) {
-        EstadoAprobacionDto saved = service.create(dto); 
+    public ResponseEntity<CiudadDto> create(@Valid @RequestBody CiudadDto dto) {
+        CiudadDto saved = service.create(dto); 
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @GetMapping("{ciudadId}") 
-    public ResponseEntity<EstadoAprobacionDto> getById(@PathVariable("ciudadId") Long id) {
-        EstadoAprobacionDto dto = service.getById(id); 
+    public ResponseEntity<CiudadDto> getById(@PathVariable("ciudadId") Long id) {
+        CiudadDto dto = service.getById(id); 
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping 
-    public ResponseEntity<List<EstadoAprobacionDto>> getAll() {
-        List<EstadoAprobacionDto> entidades = service.getAll(); 
+    public ResponseEntity<List<CiudadDto>> getAll() {
+        List<CiudadDto> entidades = service.getAll(); 
         return ResponseEntity.ok(entidades); 
     }
 
     @PutMapping("{ciudadId}") 
-    public ResponseEntity<EstadoAprobacionDto> update(@PathVariable("ciudadId") Long id,
-                                                 @RequestBody EstadoAprobacionDto updated) {
-        EstadoAprobacionDto dto = service.update(id, updated); 
+    public ResponseEntity<CiudadDto> update(@PathVariable("ciudadId") Long id,
+                                                 @RequestBody CiudadDto updated) {
+        CiudadDto dto = service.update(id, updated); 
         return ResponseEntity.ok(dto); 
     }
 
