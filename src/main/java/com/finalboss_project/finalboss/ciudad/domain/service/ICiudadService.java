f230492ea415b828a2b577a1cdcc38da838c61c5
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-17 10:44:25 -0500

    :sparkles: Safe merge from juan :sparkles:

--- src/main/java/com/finalboss_project/finalboss/ciudad/domain/service/ICiudadService.java
+++ src/main/java/com/finalboss_project/finalboss/ciudad/domain/service/ICiudadService.java
@@ -2,15 +2,15 @@ package com.finalboss_project.finalboss.ciudad.domain.service;
 
 import java.util.List;
 
-import com.finalboss_project.finalboss.ciudad.dto.EstadoAprobacionDto;
+import com.finalboss_project.finalboss.ciudad.dto.CiudadDto;
 
 
 public interface ICiudadService {
 
-    EstadoAprobacionDto create(EstadoAprobacionDto dto);
-    EstadoAprobacionDto getById(Long id);
-    List<EstadoAprobacionDto> getAll();
-    EstadoAprobacionDto update(Long id, EstadoAprobacionDto updatedDto);
+    CiudadDto create(CiudadDto dto);
+    CiudadDto getById(Long id);
+    List<CiudadDto> getAll();
+    CiudadDto update(Long id, CiudadDto updatedDto);
     void delete(Long id);
 
 }
