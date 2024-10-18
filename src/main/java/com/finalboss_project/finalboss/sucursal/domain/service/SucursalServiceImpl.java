54e1e4f8102c7cba92bfc5339db616c42d873676
JhoanSLC <JhoanSLC4@gmail.com>
2024-10-18 10:45:22 -0500

    :sparkles: Sucursal Service impl :sparkles:

---
+++ src/main/java/com/finalboss_project/finalboss/sucursal/domain/service/SucursalServiceImpl.java
@@ -0,0 +1,84 @@
+package com.finalboss_project.finalboss.sucursal.domain.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalboss_project.finalboss.customExceptions.ResourceNotFoundException;
+import com.finalboss_project.finalboss.direccion.domain.entity.Direccion;
+import com.finalboss_project.finalboss.direccion.domain.repository.IDireccionRepository;
+import com.finalboss_project.finalboss.empresa.domain.entity.Empresa;
+import com.finalboss_project.finalboss.empresa.domain.repository.IEmpresaRepository;
+import com.finalboss_project.finalboss.pais.domain.entity.Sucursal;
+import com.finalboss_project.finalboss.pais.dto.SucursalDto;
+import com.finalboss_project.finalboss.pais.mapper.SucursalMapper;
+import com.finalboss_project.finalboss.sucursal.domain.entity.Sucursal;
+import com.finalboss_project.finalboss.sucursal.domain.repository.ISucursalRepo;
+import com.finalboss_project.finalboss.sucursal.dto.SucursalDto;
+import com.finalboss_project.finalboss.sucursal.mapper.SucursalMapper;
+
+@Service
+public class SucursalServiceImpl implements ISucursalService {
+
+    @Autowired
+    private ISucursalRepo repository;
+
+    @Autowired
+    private IDireccionRepository direccionRepo;
+
+    @Autowired
+    private IEmpresaRepository empresaRepo;
+
+    @Override
+    public SucursalDto create(SucursalDto dto) {
+        Long direccionId = dto.getDireccion();
+        Long empresaId = dto.getEmpresa();
+        Direccion direccion = direccionRepo.findById(direccionId).orElseThrow(() -> new ResourceNotFoundException("Direccion con ID: "+direccionId+" no encontrado"));
+        Empresa empresa = empresaRepo.findById(empresaId).orElseThrow(() -> new ResourceNotFoundException("Empresa con ID: "+empresaId+" no encontrada"));
+        Sucursal entidad = new Sucursal(
+            dto.getId(),
+            dto.getNombre(),
+            dto.getNit(),
+            dto.getFechaCreacion(),
+            direccion,
+            empresa
+        );
+        Sucursal entidadGuardada = repository.save(entidad);
+        return SucursalMapper.toDto(entidadGuardada);
+    }
+
+    @Override
+    public SucursalDto getById(Long id) {
+        Sucursal entidad = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sucursal con el id: " + id + " no encontrado"));
+        return SucursalMapper.toDto(entidad);
+    }
+       
+
+    @Override
+    public List<SucursalDto> getAll() {
+       List<Sucursal> entidades = repository.findAll();
+       return entidades.stream()
+                .map(SucursalMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SucursalDto update(Long id, SucursalDto updatedDto) {
+        Sucursal entidad = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sucursal con el id: " + id + " no encontrado"));
+        entidad.setNombre(updatedDto.getNombre());
+        repository.save(entidad);
+        return SucursalMapper.toDto(entidad);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sucursal con el id: " + id + " no encontrado"));
+        repository.deleteById(id);
+    }
+
+}
